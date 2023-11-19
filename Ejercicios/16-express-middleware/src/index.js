2affeee9ae22967a9ddb586a79a5dec18d8461b6
JoseAntonioSegura <jaseggom@adaits.es>
2023-11-19 16:11:52 +0100

    Ejercicios terminadors

--- Ejercicios/16-express-middleware/src/index.js
+++ Ejercicios/16-express-middleware/src/index.js
@@ -26,19 +26,51 @@ const logger = createLogger({
   ],
 });
 
-server.use(morgan('combined', {
-  stream: {
-    write(message) {
-      logger.info(message);
-    },
-  },
-}));
+// Ejercicio 1
+const requestLogger = (req, res, next) => {
+  const { statusCode } = res;
+
+  if (statusCode >= 200 && statusCode < 300) {
+    // 2XX
+    logger.info(`[${new Date()}] ${req.method} ${req.originalUrl} - ${statusCode}`);
+  } else if (statusCode >= 400 && statusCode < 500) {
+    // 4XX
+    logger.warn(`[${new Date()}] ${req.method} ${req.originalUrl} - ${statusCode}`);
+  } else if (statusCode >= 500) {
+    // 5XX
+    logger.error(`[${new Date()}] ${req.method} ${req.originalUrl} - ${statusCode}`);
+  }
+
+  next();
+};
+
+// Ejercicio 2
+const validateAccess = (req, res, next) => {
+  const passwordHeader = req.headers.password;
+
+  // Verificar si el password es "patata"
+  if (passwordHeader && passwordHeader.toLowerCase() === 'patata') {
+    // Acceso correcto
+    next();
+  } else {
+    // Acceso incorrecto
+    res.status(401).json({ error: 'Acceso restringido, por favor, incluya la palabra secreta en el parámetro \'password\' en la cabecera de la petición' });
+  }
+};
 
 /* Route + Controller */
 server.get('/ping', (req, res) => {
   console.log('pong');
-  res.send('pong');
+  res.status(200).send('pong');
 });
+
+// Ejercicio 2
+server.get('/zona-restringida', validateAccess, (req, res) => {
+  res.status(200).send('Bienvenid@, disfrute del contenido.');
+});
+
+// Ejercicio 1
+server.use(requestLogger);
 
 server.listen(3000, () => {
   logger.log({ level: 'info', message: 'Server is ready' });
