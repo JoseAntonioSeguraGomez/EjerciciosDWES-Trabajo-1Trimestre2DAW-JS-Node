f325db90ff48765f88b1f0ebb42d82c9ba77092f
José Antonio Segura <132404940+JoseAntonioSeguraGomez@users.noreply.github.com>
2024-01-16 19:43:45 +0100

    Delete Trimestre-2 directory

--- Trimestre-2/24-websockets/src/app.js
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-
-const server = express();
-
-server.use(express.static('public'));
-
-export default server;
--- Trimestre-2/28-smtp-service/src/loaders/index.js
+++
@@ -1,7 +0,0 @@
-import expressLoader from './express-loader.js';
-import smtpLoader from './smtp-loader.js';
-
-export function init(server, config){
-    expressLoader(server);
-    smtpLoader(config.smtp)
-}
